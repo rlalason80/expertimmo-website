b29b0049098a2cd2fd1ae3373775deadd5c9165b
Lalason RABENIMANANA <alc@moov.mg>
2026-05-11 04:51:21 +0300

    R1 - Menage residus build et CSS

---
+++ TODO-IMAGES.md
@@ -0,0 +1,7 @@
+# Images Manquantes
+
+Liste complète des images référencées dans le code mais absentes du répertoire :
+
+- **commercial-real-estate-.jpg**
+  - Référencée dans `index.html` (ligne 1500)
+  - Référencée dans `en/index.html` (ligne 1498)
--- css/extract.ps1
+++
@@ -1,6 +0,0 @@
-$lines = Get-Content '..\index.html'
-$css1 = $lines[264..1405] -join "`n"
-$css2 = $lines[1456..1494] -join "`n"
-$all = $css1 + "`n" + $css2
-Set-Content -Path 'main.css' -Value $all -Encoding UTF8
-Write-Host "CSS extracted: $($all.Length) chars"
